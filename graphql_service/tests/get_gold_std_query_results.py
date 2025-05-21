72422a4bd4f70e207f9bda0ab1ddd204c31a30ba
Aine Fairbrother-Browne <42773574+ainefairbrother@users.noreply.github.com>
2025-05-21 13:41:17 +0100

    feat: update to import fns from test_utils

--- graphql_service/tests/get_gold_std_query_results.py
+++ graphql_service/tests/get_gold_std_query_results.py
@@ -17,145 +17,23 @@ Example usage:
 import argparse
 import asyncio
 import json
-from string import Template
-from ariadne import graphql
-from .test_utils import setup_test
+from .test_utils import execute_query
 import os
+from common.file_client import FileClient
+from graphql_service.ariadne_app import (
+    prepare_executable_schema,
+    prepare_context_provider,
+)
 
-executable_schema, context = setup_test()
-
-# -----------------------------------------------------------------------
-# Define helper functions
-
-# Define master query template
-def master_query_template():
-    """Query template"""
-    
-    query = """{
-        variant(
-            by_id: { genome_id: "$genome_id", variant_id: "$variant_id" }
-        ) {
-            name
-            alternative_names { __typename }
-            primary_source { __typename }
-            type
-            allele_type { __typename }
-            slice  { __typename }
-            alleles { 
-            name
-            allele_sequence
-            reference_sequence
-            alternative_names { __typename }
-            type
-            allele_type { __typename }
-            slice { __typename }
-            phenotype_assertions {
-                feature
-                feature_type { __typename }
-                phenotype {
-                name
-                source { __typename }
-                ontology_terms { __typename }
-                }
-                evidence {
-                source { __typename }
-                assertion { __typename }
-                }
-            }
-            prediction_results {
-                score
-                result
-                classification { __typename }
-                analysis_method { __typename }
-            }
-            population_frequencies {
-                population_name
-                allele_frequency
-                allele_count
-                allele_number
-                is_minor_allele
-                is_hpmaf
-            }
-            predicted_molecular_consequences {
-                allele_name
-                stable_id
-                feature_type { __typename }
-                consequences { __typename }
-                prediction_results {
-                score
-                result
-                classification { __typename }
-                analysis_method { __typename }
-                }
-                gene_stable_id
-                gene_symbol
-                protein_stable_id
-                transcript_biotype
-                cdna_location {
-                relation { __typename }
-                start
-                end
-                length
-                percentage_overlap
-                ref_sequence
-                alt_sequence
-                }
-                cds_location {
-                relation { __typename }
-                start
-                end
-                length
-                percentage_overlap
-                ref_sequence
-                alt_sequence
-                }
-                protein_location {
-                relation { __typename }
-                start
-                end
-                length
-                percentage_overlap
-                ref_sequence
-                alt_sequence
-                }
-            }
-            ensembl_website_display_data {
-                count_transcript_consequences
-                count_overlapped_genes
-                count_regulatory_consequences
-                count_variant_phenotypes
-                count_gene_phenotypes
-                representative_population_allele_frequency
-            }
-            }
-            prediction_results {
-            score
-            result
-            classification { __typename }
-            analysis_method { __typename }
-            }
-            ensembl_website_display_data {
-            count_citations
-            }
-        }
-    }"""
-    return Template(query)
-
-async def execute_query(genome_id, variant_id):
+def schema_and_context():
     """
-    Execute the query for a given variant_id and genome_id according to template
-    query, returning tne query string, success status, and result.
+    Prepare the GraphQL executable schema and context provider for tests.
     """
-    template = master_query_template()
-    query = template.substitute(genome_id=genome_id, variant_id=variant_id)
-    query_data = {"query": query}
-    success, result = await graphql(
-        executable_schema, query_data, context_value=context(request={})
-    )
-    return query, success, result
-
-# -----------------------------------------------------------------------
-# Generate gold standard query results
+    config = { "data_root": "/app/data" }
+    schema = prepare_executable_schema()
+    file_client = FileClient(config)
+    context = prepare_context_provider({ "file_client": file_client })
+    return schema, context
 
 async def main(variant_file, genome_id, output_dir):
     """
@@ -169,7 +47,7 @@ async def main(variant_file, genome_id, output_dir):
 
     # Run master query for each test case
     for variant_id in variants:
-        query, success, result = await execute_query(genome_id, variant_id)
+        query, success, result = await execute_query(schema_and_context, genome_id, variant_id)
         if success:
             print(f"Query executed successfully for variant {variant_id}")
             output_file = f"{output_dir}/{variant_id}.json"
@@ -185,5 +63,7 @@ if __name__ == "__main__":
     parser.add_argument("genome_id", help="Genome ID string to use in the query")
     parser.add_argument("output_dir", help="Directory to save JSON result files")
     args = parser.parse_args()
+    
+    schema_and_context = schema_and_context()
     
     asyncio.run(main(args.variant_file, args.genome_id, args.output_dir))
